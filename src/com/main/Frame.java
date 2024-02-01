14dc62dc3f01c9de686fb8981b6887b89d4dc89b
Dragonautxx <megafrizalw@gmail.com>
2024-02-01 22:44:19 +0700

    frame update

---
+++ src/com/main/Frame.java
@@ -0,0 +1,23 @@
+package com.main;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Frame {
+
+	public Frame(int width, int height, String title, Game game) {
+		
+		JFrame frame = new JFrame(title);
+		
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMaximumSize(new Dimension(width, height));
+		frame.setMinimumSize(new Dimension(width, height));
+		
+		frame.add(game);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+}
--- src/com/main/Game.java
+++ src/com/main/Game.java
@@ -1,5 +1,25 @@
 package com.main;
 
-public class Game {
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable{
     
+	private static final long serialVersionUID = 1L;
+	
+	public static final int HEIGHT = 240;
+	public static final int WIDTH = HEIGHT * 16/9;
+	
+	public Game() {
+		new Frame(HEIGHT, WIDTH, "RPG-SURVIVAL", this);
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public static void main(String args[]) {
+		new Game();
+	}
+	
 }
